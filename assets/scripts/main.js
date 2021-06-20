46cc082c2f6681dfb8d56936e0a5d0cc85221e3e
mayinx <mayinxx@web.de>
2021-06-20 22:42:56 +0200

    Add event handlers...

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,59 @@
+// TODO: Refactor the various 'toggle and hide Overlay helpers'
+// by merging them into one function each that handles explicit
+// hiding as well as toggling
 window.onload = function () {
+  "use strict";
+
   let navbar = document.getElementById("top-navbar");
-
-  /* actively hide avatar overlay if present to avoid conflicts */
-  navbar.addEventListener("click", function (e) {
-    hideAvatarOverlay();
-  });
-
-  // TODO: use id instead to avoid queryselectorall here - we won't need more than one trigger -  a permanet nav trigegr even on large screens isn't nice ...
-  document.querySelectorAll(".navbar-toggler").forEach((trigger) => {
-    trigger.addEventListener("click", function (e) {
-      navbar.classList.toggle("toggled");
-      trigger.classList.toggle("toggled");
-      e.preventDefault();
-    });
-  });
+  let navbarTrigger = document.getElementById("navbar-toggler");
+  let navOverlay = document.getElementById("nav-overlay");
 
   let avatar = document.getElementById("profile-pic");
   let heroSection = document.getElementById("page-hero");
   let learnMoreAction = document.getElementById("learn-more-action");
   let hireMeAction = document.getElementById("hire-me-action");
 
+  /* ensure that nav and nav overlay vamnishes on scroll*/
+  document.addEventListener("scroll", function () {
+    navbar.classList.remove("toggled");
+    navbarTrigger.classList.remove("toggled");
+  });
+
+  /* actively hide avatar overlay if present to avoid conflicts */
+  navbar.addEventListener("click", function (e) {
+    hideAvatarOverlay();
+  });
+
+  /* Set active nav item + toggle navbar-overlay (i.e. page-overlay caused by navbar interaction
+   * - still looking for the aappropriate wording here - so ...) */
+  document.querySelectorAll(".nav-link").forEach((navLink) => {
+    navLink.addEventListener("click", function (e) {
+      navbar.classList.toggle("toggled");
+      navbarTrigger.classList.toggle("toggled");
+    });
+  });
+
+  navOverlay.addEventListener("click", function (e) {
+    navbar.classList.toggle("toggled");
+    navbarTrigger.classList.toggle("toggled");
+  });
+
+  // TODO: use id instead to avoid queryselectorall here - we won't need more than one trigger -  a permanet nav trigegr even on large screens isn't nice ...
+  navbarTrigger.addEventListener("click", function (e) {
+    navbar.classList.toggle("toggled");
+    this.classList.toggle("toggled");
+    e.preventDefault();
+  });
+
   function toggleAvatarOverlay() {
     heroSection.classList.toggle("avatar-overlay-togggled");
-    // heroSection.classList.toggle("avatar-overlay-togggled");
+    document.body.classList.toggle("noscroll");
     console.log("-- yo - avatar overlay  togggled!");
   }
 
   function hideAvatarOverlay() {
     heroSection.classList.remove("avatar-overlay-togggled");
-    // heroSection.classList.toggle("avatar-overlay-togggled");
+    document.body.classList.remove("noscroll");
     console.log("-- yo - avatar overlay  removed!");
   }
 
@@ -62,8 +87,4 @@ window.onload = function () {
     toggleAvatarOverlay();
     handleScrolling(this, e);
   });
-
-  // window.addEventListener("click", function (e) {
-  //   toggleAvatarOverlay;
-  // });
 };
